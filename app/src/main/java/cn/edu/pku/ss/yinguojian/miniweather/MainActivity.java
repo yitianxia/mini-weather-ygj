a593651c16cb338d3cc13509fd8c2a67a118d271
yinguojian <353348253@qq.com>
2018-10-16 22:22:57 +0800

    制作选择城市界面 从数据库中读取城市数据

---
+++ app/src/main/java/cn/edu/pku/ss/yinguojian/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.ss.yinguojian.miniweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by yilen on 2018/10/6.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+//        setContentView(R.layout.activity_main);
+    }
+}
